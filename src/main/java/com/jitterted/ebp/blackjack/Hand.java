83ea2626d4a79559adb0bc891c1525ceaacb3227
Ted M. Young <tedyoung@gmail.com>
2023-09-13 10:48:55 -0700

    LAB #5: Extracted Hand class and replaced dealer and player references to use Hand.getCards

---
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -0,0 +1,16 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    private final List<Card> cards = new ArrayList<Card>();
+
+    public Hand() {
+    }
+
+    @Deprecated // this is scaffolding
+    public List<Card> getCards() {
+        return cards;
+    }
+}
